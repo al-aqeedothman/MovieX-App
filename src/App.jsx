82029bad05f6bcf8a50825820a16dc38d90690b6
alaqeedothman <alaqeedothman@gmail.com>
2024-01-08 23:03:30 +0200

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration, getGenres } from './store/homeSlice';
 import {BrowserRouter,Routes, Route} from "react-router-dom"
 
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Details from './pages/details/Details';
-import SearchResult from './pages/searchResult/searchResult';
+import SearchResult from './pages/searchResult/SearchResult';
 import Explore from './pages/explore/Explore'; 
-import PageNotFound from './pages/404/pageNotFound';
+import PageNotFound from './pages/404/PageNotFound';
 
 
 function App() {
